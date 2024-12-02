036cca8b1e70503d2f4af1eab5c5575c6a5b4991
JaiDhiman <jaidhiman2000@gmail.com>
2024-12-02 12:34:30 -0800

    basic routing and dashboard page

---
+++ src/components/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-text-primary dark:text-white">
+        Dashboard
+      </h1>
+    </div>
+  );
+}
