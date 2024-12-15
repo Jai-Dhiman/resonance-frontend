9ea0222e089397e1efd45e164d41806325fa9179
JaiDhiman <jaidhiman2000@gmail.com>
2024-12-14 18:28:32 -0800

    fixed Dockerfile and next.js config to better deploy on railway

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     domains: ["i.scdn.co"],
-  },
-  env: {
-    API_BASE_URL: process.env.API_BASE_URL || "http://localhost:3001/api",
   },
   webpack: (config) => {
     config.resolve.extensions = [".ts", ".tsx", ".js", ".jsx", ...config.resolve.extensions];
