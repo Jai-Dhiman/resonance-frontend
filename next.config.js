d3496d2b2b8d2da3e2539008cd0493bf7de34b8f
JaiDhiman <jaidhiman2000@gmail.com>
2024-12-14 18:34:07 -0800

    simplifying dockerfile/next config for sedploy

--- next.config.js
+++ next.config.js
@@ -1,20 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   images: {
     domains: ["i.scdn.co"],
   },
   webpack: (config) => {
     config.resolve.extensions = [".ts", ".tsx", ".js", ".jsx", ...config.resolve.extensions];
-    config.watchOptions = {
-      aggregateTimeout: 300,
-      poll: 1000,
-    };
     return config;
-  },
-  experimental: {
-    optimizeCss: true,
-    optimizePackageImports: ["@reduxjs/toolkit"],
   },
 };
 
